d97bff9411be7f8862661d3108b7e318d181903c
ZahzyZoraZecho <cahyo666im3.2@gmail.com>
2026-09-22 19:08:24 +0700

    fix: detect Business OS route correctly

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import BusinessOSEntry from "./BusinessOSEntry";
 import "./styles.css";
 
-const isBusinessOSRoute = window.location.pathname.replace(/\\/+$/, "").endsWith("/business-os");
+const isBusinessOSRoute = window.location.pathname.replace(/\/+$/, "").endsWith("/business-os");
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
