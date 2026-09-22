e59f13415105ceafa7a41ac722bbe39fdda8bd27
ZahzyZoraZecho <cahyo666im3.2@gmail.com>
2026-09-22 19:08:01 +0700

    fix: route Business OS through standalone login entry

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import BusinessOSEntry from "./BusinessOSEntry";
 import "./styles.css";
 
+const isBusinessOSRoute = window.location.pathname.replace(/\\/+$/, "").endsWith("/business-os");
+
 createRoot(document.getElementById("root")).render(
-  <React.StrictMode><App /></React.StrictMode>
+  <React.StrictMode>
+    {isBusinessOSRoute ? <BusinessOSEntry /> : <App />}
+  </React.StrictMode>
 );
