b735567a2b4b14cda1454f15c9e294d8d900e17b
ZahzyZoraZecho <cahyo666im3.2@gmail.com>
2026-09-22 23:00:59 +0700

    feat: route standalone Business OS applications

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import BusinessOSEntry from "./BusinessOSEntry";
+import BusinessApps from "./BusinessApps";
 import "./styles.css";
 
-const isBusinessOSRoute = window.location.pathname.replace(/\/+$/, "").endsWith("/business-os");
+const normalized = window.location.pathname.replace(/\/+$/, "");
+const isBusinessOSRoute = normalized.endsWith("/business-os");
+const appMatch = normalized.match(/\/business-os\/(pos|warehouse|sales|procurement|finance|crm|ai|commerce|erp)$/);
+const isBusinessAppRoute = Boolean(appMatch);
+
+function BusinessAppRoute() {
+  const appId = appMatch?.[1] || "launcher";
+  return <BusinessApps appId={appId} onBack={() => { window.location.href = "./"; }} />;
+}
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {isBusinessOSRoute ? <BusinessOSEntry /> : <App />}
+    {isBusinessAppRoute ? <BusinessAppRoute /> : isBusinessOSRoute ? <BusinessOSEntry /> : <App />}
   </React.StrictMode>
 );
